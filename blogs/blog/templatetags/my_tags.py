7fd3f142fc918d6d83d0be973a219000afe1df52
Igor Olenchuk <Olenchuk.io@yandex.ru>
2022-05-15 01:00:22 +0300

    Сделал фильтр is_read, в temlatetags. Переделал шаблоны index, nav, menu.

---
+++ blogs/blog/templatetags/my_tags.py
@@ -0,0 +1,17 @@
+from django import template
+
+from posts.models import ReadPost
+
+register = template.Library()
+
+
+@register.filter(name='is_read')
+def is_read(post_pk: int, request_user_pk: int) -> str:
+    """Проверяет прочитан ли пост"""
+    try:
+        ReadPost.objects.get(
+            user_id=request_user_pk,
+            post_id=post_pk)
+        return 'прочитан'
+    except ReadPost.DoesNotExist:
+        return 'не_прочитан'
