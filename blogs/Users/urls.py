ade01da7e825cf5c9362face990d73f93f22341b
Igor Olenchuk <Olenchuk.io@yandex.ru>
2022-05-14 23:39:40 +0300

    Удалил приложение Users, создал Templates в приложении blog, содал мщдели в приложении blog

--- blogs/Users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # в задании сказано, что регистрация пользователя не обязательно, тем не менее
-    path("signup/", views.SignUp.as_view(), name="signup"),
-]
