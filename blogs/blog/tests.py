e23def1b08e9142a8f07c357b2e74ebfce7776be
Igor Olenchuk <Olenchuk.io@yandex.ru>
2022-05-15 19:17:52 +0300

    add test blog

--- blogs/blog/tests.py
+++ blogs/blog/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from http import HTTPStatus
+from django.test import Client, TestCase
 
-# Create your tests here.
+
+class StaticPagesURLTests(TestCase):
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_about_url_exists_at_desired_location(self):
+        """Проверка доступности страниц."""
+        response = self.guest_client.get('/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        response = self.guest_client.get('/myposts/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_page_shows_correct_content(self):
+        """Проверка контента страниц."""
+        response = self.guest_client.get('/')
+        self.assertContains(response, 'У меня получилось!')
+
+        response = self.guest_client.get('/myposts/')
+        self.assertContains(response, 'А страница моих постов')
