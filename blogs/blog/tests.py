b3dd9db58e49ffa12857e75a9c5a267397f60373
Igor Olenchuk <Olenchuk.io@yandex.ru>
2022-05-15 22:43:24 +0300

    add test blog

--- blogs/blog/tests.py
+++ blogs/blog/tests.py
@@ -8,7 +8,7 @@ class StaticPagesURLTests(TestCase):
 
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
-        response = self.guest_client.get('/')
+        response = self.guest_client.get('/news-feed/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
         response = self.guest_client.get('/news-feed/')
@@ -16,7 +16,7 @@ class StaticPagesURLTests(TestCase):
 
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
-        response = self.guest_client.get('/')
+        response = self.guest_client.get('/news-feed/')
         self.assertContains(response, 'У меня получилось!')
 
         response = self.guest_client.get('/news-feed/')
